1b5decec160b710d88190c7828e3d95f47ef7bbe
vanildo <silvavs.ti@gmail.com>
2018-02-27 20:32:32 -0300

    teste01

---
+++ 02/op-relacionais.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "<pre>";
+
+//Igual
+
+var_dump(2 == 2);
+var_dump(2 === "2");
+echo "<hr>";
+
+//menor
+
+var_dump(2 < 5);
+var_dump(2 > 2);
+var_dump(2 > 2);
+echo "<hr>";
+
+// Maior ou igual
+
+var_dump(2 >= 5);
+var_dump(5 >= 2);
+var_dump(2 >- 2);
+echo "<hr";
