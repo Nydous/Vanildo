7e9ffaf6a455d1151cbe6fc8c3d5aace0fbfa927
vanildo <silvavs.ti@gmail.com>
2018-02-27 21:17:41 -0300

    teste2

---
+++ 02/heredoc.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<pre>";
+
+$nome = "vanildo";
+
+$texto = <<< TXT
+asdvkjsdnfkj
+sdsfdndsl
+afnlkasdf
+$nome
+dnfkls
+TXT;
+
+echo $texto;
---
+++ 02/string.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<pre>";
+$a = "1,2,3";
+
+echo "testando $a";
+echo "<br>";
+echo 'Testando $a';
+
+echo "<hr>";
+
+echo "Testando " , $a , ' !!';
